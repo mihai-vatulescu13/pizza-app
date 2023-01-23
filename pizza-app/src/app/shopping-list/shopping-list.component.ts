0b292227c8b0206574808057ebbeabbf6f43dff3
mihai vatulescu <vatulescu.mihai@gmail.com>
2023-01-23 22:18:47 +0200

    Added form

--- pizza-app/src/app/shopping-list/shopping-list.component.ts
+++ pizza-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  public ingredients: Ingredient[] = [
+    {
+      name: 'Apples',
+      amount: 3,
+    },
+    {
+      name: 'Tomatoes',
+      amount: 10,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
